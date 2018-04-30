a647497b3ddfd4ee23ab0a483467473de0549c9c
ik80dev <ik80dev@gmail.com>
2018-04-30 13:45:50 +0200

    Usunąłem dodatkową spację.

--- decorators/example_00.py
+++ decorators/example_00.py
@@ -1,6 +1,5 @@
 #-*-coding:utf-8-*-
 
-
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #	Autor: IK.
 #	Opis: Przykład użycia dekoratorów w języku skryptowym Python.

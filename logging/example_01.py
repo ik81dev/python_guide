d31d5edeb2a5080d80622f3de42fe049610aaa54
ik80dev <ik80dev@gmail.com>
2018-05-07 18:36:08 +0200

    Przykład wykorzystania modulu logging.

--- logging/example_01.py
+++ logging/example_01.py
@@ -3,31 +3,32 @@ import os
 import sys
 import logging
 import unittest
+import argparse
 
 """
 	Description: 
-
+		
 	Author: 
 	Wersion:
 	Date:
 """
-
 
 module_logger = logging.getLogger(__name__)
 module_logger.debug("Module mame %s", __name__)
 
 class Program(object):
 	"""Sample """
-	def __init__(self, logger=None)
-		self.logger = logger or logging.getLogger(__name__)
-	
+	def __init__(self, logger=None):
+		self.logger = logger or logging.getLogger(Program.__name__)
+		self.logger.debug("Konstruktor...")
 	
 	def run(self):
+		self.logger.debug("run")
 		
 		
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.DEBUG)
-	
+	module_logger.debug("Module logger...")
 	parser = argparse.ArgumentParser(description='Description')
 	"""Positional args"""
 	#parser.add_argument('integers', metavar='int', nargs='+', type=int, help='an integer to be summed')

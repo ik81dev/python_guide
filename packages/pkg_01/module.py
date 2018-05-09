633dfc66230de09150ac637cb5d1c182c50bb89a
ik80dev <ik80dev@gmail.com>
2018-05-09 13:25:59 +0200

    Moduł...

--- packages/pkg_01/module.py
+++ packages/pkg_01/module.py
@@ -1,11 +1,13 @@
 #	Wersje modulu:
 #		1.0.0 
-#				- Implementacja funkcji dodajacej dwie liczby
-#
-#
+#				- Implementacja funkcji dodajacej dwie liczby.
+#		1.0.1
+#				- Implementacja mechanizmu sprawdzania typu argumentow przekazywanych do funkci `add_integers`.
 #
 
 def add_integers(a, b):
 	"""Funkcja dodaje a do b i zwraca wynik"""
-	return a + b
-	
+	if isinstance(a, int) and isinstance(b, int):
+		return a + b
+	else:
+		raise TypeError("Argumenty funkcji musza byc typu int!")
